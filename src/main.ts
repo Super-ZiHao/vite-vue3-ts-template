52d3543e9120ac0610d3324383224f6a48485f3a
v_hzihhuang <v_hzihhuang@tencent.com>
2023-02-27 12:22:31 +0800

    perf: 使用sass、配置别名、配置路由、配置状态

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,10 @@
 import { createApp } from 'vue'
-import './style.css'
+import './style.scss'
 import App from './App.vue'
+import route from '@/router';
+import { createPinia } from 'pinia';
 
-createApp(App).mount('#app')
+createApp(App)
+.use(route)
+.use(createPinia())
+.mount('#app')
---
+++ src/pages/Home.vue
@@ -0,0 +1,15 @@
+<script lang='ts' setup>
+import Test from '@/components/Test.vue';
+import { useStore } from '@/store';
+const store = useStore();
+</script>
+
+<template>
+  <div>Home</div>
+  <Test />
+  <div>{{ store.text }}</div>
+</template>
+
+<style lang='scss' scoped>
+
+</style>
